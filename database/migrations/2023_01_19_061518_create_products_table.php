2d0f810a9999510d7a25e950949c96cfe5ae7a88
MeisyAnjarfika <meisyanjarfika@student.uns.ac.id>
2023-01-19 13:53:46 +0700

    update product

--- database/migrations/2023_01_19_061518_create_products_table.php
+++ database/migrations/2023_01_19_061518_create_products_table.php
@@ -15,7 +15,19 @@ return new class extends Migration
     {
         Schema::create('products', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('brand_id')->nullable()->constrained();
+            $table->string('sku')->index();;
+            $table->string('name');
+            $table->decimal('price', 15, 2)->nullable();
+            $table->integer('stock')->default(0);
             $table->timestamps();
+        });
+
+
+        Schema::create('category_product', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('category_id')->constrained();
+            $table->foreignId('product_id')->constrained();
         });
     }
 
@@ -26,6 +38,7 @@ return new class extends Migration
      */
     public function down()
     {
+        Schema::dropIfExists('category_product');
         Schema::dropIfExists('products');
     }
 };
