5c3c10a7dabda3ff3f818715367325e67f21cbbe
MeisyAnjarfika <meisyanjarfika@student.uns.ac.id>
2023-01-19 13:51:08 +0700

    update create

--- database/migrations/2023_01_19_061222_create_brands_table.php
+++ database/migrations/2023_01_19_061222_create_brands_table.php
@@ -15,6 +15,8 @@ return new class extends Migration
     {
         Schema::create('brands', function (Blueprint $table) {
             $table->id();
+            $table->string('slug')->unique();
+            $table->string('name');
             $table->timestamps();
         });
     }
